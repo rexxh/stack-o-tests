190c0d81dec36c4deb0e41a03f6174f97945f3e9
GavrilovN <coinflipper97@yandex.ru>
2016-09-18 21:34:45 +0300

    Update Source.cpp

--- sources/Source.cpp
+++ sources/Source.cpp
@@ -1,15 +1,22 @@
 #include "Header.h"
 
-template <typename T> //освобождаем памяти
+template <typename T>//РєРѕРїРёСЂРѕРІР°РЅРёРµ Рё РІС‹РґРµР»РµРЅРёРµ РїР°РјСЏС‚Рё 
+auto stack<T>::mem_copy(size_t count_m, size_t array_size_m, const T * tmp)->T* {
+	T *mass = new T[array_size_m];
+	copy(tmp,tmp+count_m,mass);
+	return mass; 
+}
+
+template <typename T> 
 inline stack<T>::~stack()
 {
 	delete[] array_;
 }
 
-template <typename T> //конструктор по умолчанию
+template <typename T> 
 inline stack<T>::stack() :count_(0), array_size_(0), array_(nullptr) {};
 
-template <typename T> //вставляем эл-нт в стэк 
+template <typename T> 
 inline auto stack<T>::push(T const &com)->void {
 	if (array_ == nullptr) {
 		array_ = new T[1];
@@ -18,11 +25,10 @@ inline auto stack<T>::push(T const &com)->void {
 	}
 	else {
 		if (count_ == array_size_) {
-			T *tmp = array_;
 			array_size_ *= 2;
-			array_ = new T[array_size_];
-			std::copy(tmp, tmp + count_, array_);
-			delete[] tmp;
+			T *tmp = mem_copy(count_,array_size_,array_);
+			delete[] array_;
+			array_ = tmp;
 
 		}
 		array_[count_] = com;
@@ -31,13 +37,11 @@ inline auto stack<T>::push(T const &com)->void {
 	}
 }
 
-template <typename T> //конструктор копирования
-inline stack<T>::stack(const stack&tmp) :count_(tmp.count_), array_size_(tmp.array_size_), array_(new T[tmp.array_size_]) {
-	array_ = new T[array_size_];
-	copy(tmp.array_, tmp.array_ + count_, array_);
-}
+template <typename T> 
+inline stack<T>::stack(const stack&tmp) :count_(tmp.count_), array_size_(tmp.array_size_),array_(copy(tmp.count_, tmp.array_size, tmp.array_)) {}
+	
 
-template <typename T> //перегружаем оператор присваивания 
+template <typename T>  
 inline auto stack<T>::operator=(const stack&tmp)->stack& {
 	if (this != &tmp) {
 		delete[] array_;
@@ -49,23 +53,18 @@ inline auto stack<T>::operator=(const stack&tmp)->stack& {
 	return *this;
 }
 
-template <typename T> // выводим стэк
-inline auto stack<T>::print()->void {
-	for (size_t i(0); i < count_; i++) cout << array_[i] << endl;
-}
-
-template <typename T> //возвращаем count_
+template <typename T> 
 inline auto stack<T>::count() const noexcept->size_t {
 	return count_;
 }
 
-template <typename T> // уменьшаем count_ 
+template <typename T> 
 inline auto stack<T>::pop()->T {
 	if (count_ == 0) throw logic_error("Empty!");
 	return --count_;
 }
 
-template <typename T> //удаляем эл-нт
+template <typename T> 
 inline auto stack<T>::top() const->T& {
 	if (count_ == 0) throw logic_error("Empty!");
 	return array_[count_];

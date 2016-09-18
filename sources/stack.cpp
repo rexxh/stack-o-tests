2760c387c2c594d9e43bf073e3f0a920add7950e
GavrilovN <coinflipper97@yandex.ru>
2016-09-18 23:06:52 +0300

    Update stack.cpp

--- sources/stack.cpp
+++ sources/stack.cpp
@@ -1,7 +1,7 @@
 #include "stack.h"
 
 template <typename T>//копирование и выделение памяти 
-auto stack<T>::mem_copy(size_t count_m, size_t array_size_m, const T * tmp)->T* {
+auto mem_copy(size_t count_m, size_t array_size_m, const T * tmp)->T* {
 	T *mass = new T[array_size_m];
 	copy(tmp,tmp+count_m,mass);
 	return mass; 
@@ -18,12 +18,6 @@ inline stack<T>::stack() :count_(0), array_size_(0), array_(nullptr) {};
 
 template <typename T> 
 inline auto stack<T>::push(T const &com)->void {
-	if (array_ == nullptr) {
-		array_ = new T[1];
-		array_[0] = com;
-		count_++; array_size_++;
-	}
-	else {
 		if (count_ == array_size_) {
 			array_size_ *= 2;
 			T *tmp = mem_copy(count_,array_size_,array_);
@@ -35,10 +29,9 @@ inline auto stack<T>::push(T const &com)->void {
 		count_++;
 
 	}
-}
 
 template <typename T> 
-inline stack<T>::stack(const stack&tmp) :count_(tmp.count_), array_size_(tmp.array_size_),array_(copy(tmp.count_, tmp.array_size, tmp.array_)) {}
+inline stack<T>::stack(const stack&tmp) :count_(tmp.count_), array_size_(tmp.array_size_), array_(mem_copy(tmp.count_, tmp.array_size_, tmp.array_)) {}
 	
 
 template <typename T>  

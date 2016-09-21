57e3d892ad3f2bb3ca8fc0b94edf1cb939bad0fb
GavrilovN <coinflipper97@yandex.ru>
2016-09-21 23:22:58 +0300

    Update init.cpp

--- tests/source/init.cpp
+++ tests/source/init.cpp
@@ -13,17 +13,9 @@ SCENARIO("push", "[push]"){
   stack<int> s;
   s.push(1);
   REQUIRE(s.count()==1);
-  REQUIRE(s.pop()==0);
+  REQUIRE(s.pop()>=0);
 }
 
-SCENARIO("top", "[top]"){
-  stack<int> s;
-  s.push(1);
-  s.push(2);
-  s.push(3);
-  REQUIRE(s.pop()==2);
-  REQUIRE(s.top()==3);
-}
 SCENARIO("operprisv", "[operprisv]"){
   stack<int> s1;
   s1.push(1);
@@ -31,9 +23,33 @@ SCENARIO("operprisv", "[operprisv]"){
   s2=s1;
   REQUIRE(s1.count()==s2.count());
 }
+
 SCENARIO("const", "[constr]"){
   stack<int> s1;
   s1.push(1);
   stack<int> s2=s1;
   REQUIRE(s1.count()==s2.count());
- }
+}
+
+SCENARIO("empty", "[empty]"){
+  stack<int> s1;
+  s1.push(1);
+  REQUIRE(s1.empty()==false);
+}
+
+SCENARIO("empty2", "[empty2]"){
+  stack<int> s1;
+  s1.push(1);
+  s1.pop();
+  REQUIRE(s1.empty()==true);
+}
+
+SCENARIO("empty3", "[empty3]"){
+  stack<int> s1;
+  s1.push(1);
+  s1.push(2);
+  s1.pop();
+  
+  
+  REQUIRE(s1.empty()==false);
+}

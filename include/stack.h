4f1c111fbfd9f87bd85effe5391f870804e6ebe2
GavrilovN <coinflipper97@yandex.ru>
2016-09-21 23:24:25 +0300

    Update stack.h

--- include/stack.h
+++ include/stack.h
@@ -10,7 +10,6 @@ public:
 	auto count() const noexcept->size_t;/*noexcept*/
 	auto push(T const &)->void;/*strong*/
 	auto pop()->T;/*strong*/
-	auto top() const->T&;/*strong*/
 	~stack(); 	/*noexcept*/
 	auto operator=(const stack&tmp)->stack&;/*strong*/
 	auto empty()->bool;	/*noexcept*/

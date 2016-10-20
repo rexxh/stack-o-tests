c85c4739e57679c2525d0aa35342aa5adf26c429
GavrilovN <coinflipper97@yandex.ru>
2016-10-20 22:47:15 +0300

    Update stack.h

--- include/stack.h
+++ include/stack.h
@@ -1,22 +1,74 @@
 #include <iostream>
 using namespace std;
 
+template<typename T>
+class allocator
+{
+protected:
+	allocator(size_t size = 0);
+	~allocator();
+	auto swap(allocator& other)->void;
+	allocator(allocator const&) = delete;
+	auto operator=(allocator const&)->allocator& = delete;
+	T * array_;
+	size_t size_;
+	size_t count_;
+};
+
+//__________________________________________________________________________________________________________________
+//__________________________________________________________________________________________________________________
+
+
+template <typename T1, typename T2>
+auto construct(T1 * array_, T2 const & value)->void {
+	new(array_) T1(value);
+}
+
 template <typename T>
-class stack
+void destroy(T * array_) noexcept
+{
+	array_->~T();
+}
+
+template <typename FwdIter>
+void destroy(FwdIter first, FwdIter last) noexcept
+{
+	for (; first != last; ++first) {
+		destroy(&*first);
+	}
+}
+
+template <typename T>// êîíñòðóêòîð àëëîêàòîðà
+allocator<T>::allocator(size_t size) : ptr_(static_cast<T *>(size == 0 ? nullptr : operator new(size * sizeof(T)))), size_(0), count_(size) {
+};
+
+template <typename T>//äåñòðóêòîð àëëîêàòîðà
+allocator<T>::~allocator() {
+operator delete(array_);
+};
+
+template <typename T>//swap allocator
+auto allocator<T>::swap(allocator& other)->void {
+	std::swap(array_, other.array_);
+	std::swap(size_, other.size_);
+	std::swap(count_, other.count_);
+};
+
+//__________________________________________________________________________________________________________________
+//__________________________________________________________________________________________________________________
+ 
+template <typename T>
+class stack : private allocator<T>
 {
 public:
 	stack();/*noexcept*/
-	stack(const stack&); /*strong*/
+	stack(stack const &); /*strong*/
 	auto count() const noexcept->size_t;/*noexcept*/
 	auto push(T const &)->void;/*strong*/
-	auto pop()->void; /*strong*/
+	auto pop()->void;/*strong*/
 	auto top() const->T&;/*strong*/
 	~stack(); 	/*noexcept*/
 	auto operator=(const stack&tmp)->stack&;/*strong*/
-	auto empty()->bool;	/*noexcept*/
-private:	
-	T * array_;
-	size_t array_size_;
-	size_t count_;
-	auto swap(stack & tmp) -> void; /*noexcept*/
+	auto empty()->bool;/*noexcept*/
+
 };

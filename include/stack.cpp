8cf455d36c4a3205da5a7810273b4326a4c0f8c1
GavrilovN <coinflipper97@yandex.ru>
2016-10-20 22:50:20 +0300

    Update stack.cpp

--- include/stack.cpp
+++ include/stack.cpp
@@ -1,82 +1,76 @@
 #include "stack.h"
 
+template<typename T> 
+auto stack<T>::empty()->bool {
+	return (allocator<T>::count_ == 0);
+}
+
 template <typename T>
 auto mem_copy(size_t count_m, size_t array_size_m, const T * tmp)->T* {
 	T *mass = new T[array_size_m];
-	try{
-		std::copy(tmp,tmp+count_m,mass);
-	}
-	catch(...){
-	
-		delete[] mass;
-	
-		throw;
-	}
-	return mass; 
+	std::copy(tmp, tmp + count_m, mass);
+	return mass;
 }
 
-template<typename T>
-inline auto stack<T>::empty()->bool {
-	return (count_ == 0);
-}
-template <typename T>
-inline stack<T>::~stack()
-{
-	delete[] array_;
-}
 
 template <typename T>
-inline stack<T>::stack() :count_(0),array_size_(0),array_(nullptr){};
+stack<T>::~stack() {
+destroy(allocator<T>::array_, allocator<T>::array_ + allocator<T>::count_);};
 
 template <typename T>
-inline auto stack<T>::push(T const &val)->void {
-	if (count_ == array_size_) {
-			size_t size = array_size_ * 2+ (array_size_==0);
-			T *tmp = mem_copy(count_,size,array_);
-			delete[] array_;
-			array_ = tmp;
-			array_size_=size;
+stack<T>::stack()  {};
 
-		}
-		array_[count_] = val;
-		count_++;
-	
+
+
+template <typename T>
+auto stack<T>::push(T const &val)->void {
+	if (allocator<T>::count_ == allocator<T>::size_) {
+		size_t size = allocator<T>::size_ * 2 + (allocator<T>::size_ == 0);
+		T *tmp = mem_copy(allocator<T>::count_, size, allocator<T>::array_);
+		delete[] allocator<T>::array_;
+		allocator<T>::array_ = tmp;
+		allocator<T>::size_ = size;
 	}
+	construct(allocator<T>::array_+allocator<T>::count_,val);
+	++allocator<T>::count_;
+}
 
 
 template <typename T>
-inline stack<T>::stack(const stack&tmp) :count_(tmp.count_), array_size_(tmp.array_size_), array_(mem_copy(tmp.count_, tmp.array_size_, tmp.array_)) {}
+stack<T>::stack(stack const &tmp) {
+	allocator<T>::count_ = tmp.count_;
+	allocator<T>::array_ = mem_copy(tmp.count_, tmp.size_, tmp.array_);
+	allocator<T>::size_ = tmp.size_;
+};
 
 template <typename T>
-inline auto stack<T>::operator=(const stack &tmp)->stack& {
+auto stack<T>::operator=(const stack &tmp)->stack& {
 	if (this != &tmp) {
-		(stack(tmp)).swap(*this);
+		T* cp = mem_copy(tmp.count_, tmp.size_, tmp.array_);
+		delete[] allocator<T>::array_; 
+		allocator<T>::array_ = cp;
+		allocator<T>::size_ =tmp.size_;
+		allocator<T>::count_ = tmp.count_;
 	}
 	return *this;
 }
 
-template<typename T>
-auto stack<T>::swap(stack & tmp) -> void {
-	std::swap(tmp.array_size_, array_size_);
-	std::swap(tmp.array_, array_);
-	std::swap(tmp.count_, count_);
+
+template <typename T>
+auto stack<T>::count() const noexcept->size_t {
+	return allocator<T>::count_;
 }
 
 template <typename T>
-inline auto stack<T>::count() const noexcept->size_t {
-	return count_;
-}
-
-
-template <typename T> 
-inline auto stack<T>::pop()->void {
-	if (count_ == 0) throw std::logic_error("Empty!");
-	 --count_;
+auto stack<T>::pop()->void {
+	if (allocator<T>::count_ == 0) throw std::logic_error("Empty!");
+	destroy(allocator<T>::array_+allocator<T>::count_);
+	--allocator<T>::count_;
 }
 
 template <typename T>
-inline auto stack<T>::top() const->T& {
-	if (count_ == 0) throw std::logic_error("Empty!");
-	return array_[count_];
+auto stack<T>::top() const->T& {
+	if (allocator<T>::count_ == 0) throw std::logic_error("Empty!");
+	return allocator<T>::array_[allocator<T>::count_-1];
 
 }
